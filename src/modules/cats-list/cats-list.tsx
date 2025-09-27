e2f58f6b0ef85a52150e8cea4f65342fe11dcb43
tBaronDar <thedarelis@gmail.com>
2025-09-27 16:07:43 +0300

    fixed prettierrc systax, worked on cats-list

--- src/modules/cats-list/cats-list.tsx
+++ src/modules/cats-list/cats-list.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Stack } from '@mui/material';
 import useCatsList from './hooks/use-cats-list';
+import { type Cat } from '../../shared/dto/cat-read';
 
-const CatsList: React.FC = () => {
-  const { data, isLoading, error } = useCatsList();
-  console.log(data, isLoading, error);
+export type CatsListProps = {
+  cats: Array<Cat>;
+  isLoading: boolean;
+  // error: Error | null;
+};
+
+const CatsListSkeleton = () => {
   return (
     <Box>
       <Typography variant='h4' component='h1' gutterBottom>
         Random Cats
       </Typography>
-      <Typography variant='body1' color='text.secondary'>
-        Welcome to CatLover! Here you&apos;ll see random cat images.
-      </Typography>
     </Box>
   );
+};
+
+const CatsListInner = (props: CatsListProps) => {
+  const { cats, isLoading } = props;
+
+  console.log(cats, isLoading);
+  return (
+    <Stack>
+      <Box>
+        <Typography variant='h4' component='h1' gutterBottom>
+          Random Cats
+        </Typography>
+        <Typography variant='body1' color='text.secondary'>
+          Welcome to CatLover! Here you&apos;ll see random cat images.
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+const CatsList: React.FC = () => {
+  const { cats, isLoading } = useCatsList();
+  return isLoading ? <CatsListSkeleton /> : <CatsListInner cats={cats} isLoading={isLoading} />;
 };
 
 export default CatsList;
--- src/modules/cats-list/hooks/use-cats-list.ts
+++ src/modules/cats-list/hooks/use-cats-list.ts
@@ -1,5 +1,8 @@
 import api from '../../../shared/services/query/api';
 export default function useCatsList() {
   const { data, isLoading, error } = api.useGetRandomCatsQuery({ limit: 10 });
-  return { data, isLoading, error };
+
+  const cats = data ?? [];
+
+  return { cats, isLoading, error };
 }

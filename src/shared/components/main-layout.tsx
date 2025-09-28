bdfc87f5d5771e9cbeba83ace36468b275dcf4de
tBaronDar <thedarelis@gmail.com>
2025-09-28 13:21:45 +0300

    made sure guest exists in the appbarr

--- src/shared/components/main-layout.tsx
+++ src/shared/components/main-layout.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import CustomAppContainer from './custom-app-contaner';
 import CustomAppBar from './custom-app-bar';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import type { RootState } from '../../config/store';
+import { ensureGuestExists } from '../reducers/app.reducer';
 
 const Layout: React.FC = () => {
+  const dispatch = useDispatch();
   const guest = useSelector((state: RootState) => state.app.guest);
+
+  useEffect(() => {
+    if (!guest) {
+      dispatch(ensureGuestExists());
+    }
+  }, [guest, dispatch]);
+
+  if (!guest) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <CustomAppContainer>
